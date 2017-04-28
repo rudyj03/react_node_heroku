bf7115802f32f52768e1e18593b30b8d82feec00
Rudy Jadeja <rudyj03@gmail.com>
2017-04-27 21:45:01 -0400

    dumb stuff

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import darn from './darn.jpg';
 import './style.css';
 
 class App extends Component {
@@ -8,10 +9,11 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <img src={darn} className="App-Darn" alt="darn" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/components/App/index.js</code> and save to reload.
+          <h1> Shal, I have too much time on my hands. </h1>
         </p>
       </div>
     );
